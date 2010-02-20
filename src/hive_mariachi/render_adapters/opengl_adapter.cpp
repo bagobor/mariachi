c0aa638d4295afef4261039822ad04f24578d183
joamag <joamag@gmail.com>
2010-02-20 23:18:11 +0000

    added todo for backface culling

--- src/hive_mariachi/render_adapters/opengl_adapter.cpp
+++ src/hive_mariachi/render_adapters/opengl_adapter.cpp
@@ -124,6 +124,10 @@ void OpenglAdapter::init() {
     // enables the support for 2d textures
     glEnable(GL_TEXTURE_2D);
 
+    // enables backface culling support
+    // @todo add support for normals (in models) to be able to use this
+    //glEnable(GL_CULL_FACE);
+
     // select clearing (background) color
     glClearColor(0.0, 0.0, 0.0, 0.0);
 
